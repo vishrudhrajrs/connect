2ecb06d2bd33cc650fd2e06784fd2576ae1ea233
vishrudhrajrs <vishrudhrajrs14@gmail.com>
2021-05-23 10:21:48 +0530

    test

--- app.py
+++ app.py
@@ -1,16 +1,5 @@
 from Connect import app, db
 
 if __name__ == '__main__':
-    # for i in dbx.files_list_folder("").entries:
-    #     if os.path.exists("/app/Connect/static/img/uploads"+i.path_lower):
-    #         _,f=dbx.files_download("/img1.png")
-    #         print("success")
-
-    #         f = f.content
-
-    #         print(type(f))
-    #         img = Image.open(io.BytesIO(f))
-    #         img.save("/app/Connect/static/img/uploads"+i.path_lower)
-
-        app.run(debug=True)
+    app.run(debug=True)
 
